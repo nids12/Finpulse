027e995a7179b334bd1e68cd3e7f85399d15cca8
Nidhi Sarda <130972976+nids12@users.noreply.github.com>
2026-07-17 11:30:18 +0530

    Remove comments from portfolioRoutes.js

--- backend/routes/portfolioRoutes.js
+++ backend/routes/portfolioRoutes.js
@@ -3,11 +3,8 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const portfolioController = require("../controllers/portfolioController");
 
-// Buy stock
 router.post("/buy", auth, portfolioController.buyStock);
-// Sell stock
 router.post("/sell", auth, portfolioController.sellStock);
-// Get portfolio
 router.get("/portfolio", auth, portfolioController.getPortfolio);
 
 module.exports = router;
